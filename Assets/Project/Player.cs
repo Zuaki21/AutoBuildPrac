b2a7e39827a8b929d2797c5f17101fe553a5fc61
Zuaki <67538904+Zuaki21@users.noreply.github.com>
2023-03-24 00:36:14 +0900

    Playerが自由に動ける

--- Assets/Project/Player.cs
+++ Assets/Project/Player.cs
@@ -21,6 +21,12 @@ public class Player : MonoBehaviour
             rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
             isGrounded = false;
         }
+
+        //左右移動Input.GetAxis("Horizontal")で-1~1の値を取得
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * 3, 0, 0) * Time.deltaTime;
+
+        //移動可能な範囲は画面内に制限Mathf.Clamp
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -8f, 8f), transform.position.y, transform.position.z);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -28,7 +34,6 @@ public class Player : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             isGrounded = true;
-            
         }
     }
 }
