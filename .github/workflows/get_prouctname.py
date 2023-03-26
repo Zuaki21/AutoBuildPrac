7a5e7ef1afa41d5ccf6132b60f5c524e51b6f918
Zuaki <67538904+Zuaki21@users.noreply.github.com>
2023-03-26 23:03:05 +0900

    バージョン情報を自動取得するよう改変

--- .github/workflows/get_prouctname.py
+++ .github/workflows/get_prouctname.py
@@ -1,14 +1,22 @@
 import sys
-import yaml
+import re
 
 
 def get_product_name(file_path):
     # YAMLファイルの読み込み
-    with open(file_path, 'r') as file:
-        settings = yaml.safe_load(file)
+    with open(file_path, 'r') as f:
+        contents = f.read()
 
     # productNameの文字列を取得
-    product_name = settings['PlayerSettings']['productName']
+    product_name = ""
+
+    pattern = r'productName:\s+(\S+)\n'
+    match = re.search(pattern, contents)
+    if match:
+        product_name = match.group(1)
+        #もし、"で囲まれていたら、"を削除
+        if product_name.startswith('"') and product_name.endswith('"'):
+            product_name = product_name[1:-1]
 
     # Unicodeエスケープシーケンスをデコードして文字列に変換
     decoded_product_name = bytes(
@@ -16,3 +24,12 @@ def get_product_name(file_path):
 
     # 結果を返す
     return decoded_product_name
+
+
+if __name__ == "__main__":
+    # コマンドライン引数を取得
+    args = sys.argv
+    # 引数があれば、実行
+    if len(args) > 1:
+        product_name = get_product_name(args[1])
+        print(product_name)
